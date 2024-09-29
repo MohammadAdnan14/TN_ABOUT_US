44990ad664cfae41a0a086f12e7b58453766fd30
Adnan <studentprogrammer3.com>
2024-09-29 17:02:37 +0530

    #

---
+++ about-us/src/app/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+import Landingpage from "./main";
+
+export default function Home() {
+  return (
+    <Landingpage/>
+    // <div>This is my about us page</div>
+  );
+}
